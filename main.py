e05f060e0e2efed932579de0f81aa48e3d119376
Sergio Peralta <seperalta88@gmail.com>
2023-06-02 10:42:40 -0300

    document the Api

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 app= FastAPI()
+app.title = "My First API with FastAPI"
+app.version = "0.0.1"
+app.description = "This is my first API with FastAPI"
 
-@app.get("/")
+@app.get("/" ,tags=["Root"])
 def message():
     return {"message": "Hello World"}
