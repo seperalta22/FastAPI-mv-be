72414e02682595c340286c408c527a498ddf6c19
Sergio Peralta <seperalta88@gmail.com>
2023-06-02 18:56:11 -0300

    Add%20patch%20method

--- main.py
+++ main.py
@@ -71,3 +71,15 @@ def create_movie(
     }
     movies.append(new_movie)
     return new_movie
+
+@app.patch("/api/movies/{id}", tags=["Movies"])
+
+def update_movie(id: int, name: str = Body(None), casts: list = Body(None), genres: list = Body(None)):
+    for movie in movies:
+        if(movie["id"] == id):
+            movie["name"] = name if name is not None else movie["name"]
+            movie["casts"] = casts if casts is not None else movie["casts"]
+            movie["genres"] = genres if genres is not None else movie["genres"]
+            return movie
+    return {"Error": "Movie not found"}
+
