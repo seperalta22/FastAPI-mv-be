077a8f33ab08f684594dd97550387a9cecdb6108
Sergio Peralta <seperalta88@gmail.com>
2023-06-02 21:39:48 -0300

    add types

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import Body, FastAPI
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 app= FastAPI()
 app.title = "My First API with FastAPI"
 app.version = "0.0.1"
 app.description = "This is my first API with FastAPI"
+
+class Movie(BaseModel):
+    id: int | None
+    name: str
+    casts: list
+    genres: list
 
 movies = [
     {
@@ -51,36 +58,21 @@ def get_movies_by_genre(genre: str):
     for movie in movies:
         if(movie["genres"].count(genre) > 0):
             results.append(movie)
-
     if len(results) == 0:
         return {"Error": "Genre not found"}
     return results
     
 @app.post("/api/movies", tags=["Movies"])
-def create_movie(
-    id: int = Body(), 
-    name: str= Body(), 
-    casts: list = Body(), 
-    genres: list = Body()
-):
-    new_movie = {
-        "id": id,
-        "name": name,
-        "casts": casts,
-        "genres": genres
-    }
-    movies.append(new_movie)
-    return new_movie
+def create_movie(movie: Movie):
+    movies.append(movie)
+    return {"Success": "Movie created successfully"}
 
-@app.patch("/api/movies/{id}", tags=["Movies"])
-
-def update_movie(id: int, name: str = Body(None), casts: list = Body(None), genres: list = Body(None)):
-    for movie in movies:
-        if(movie["id"] == id):
-            movie["name"] = name if name is not None else movie["name"]
-            movie["casts"] = casts if casts is not None else movie["casts"]
-            movie["genres"] = genres if genres is not None else movie["genres"]
-            return movie
+@app.put("/api/movies/{id}", tags=["Movies"])
+def update_movie(id: int, movie: Movie):
+    for i in range(len(movies)):
+        if(movies[i]["id"] == id):
+            movies[i] = movie
+            return {"Message": "Movie updated successfully"}
     return {"Error": "Movie not found"}
 
 
