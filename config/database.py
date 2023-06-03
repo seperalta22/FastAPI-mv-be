7eef2181acc3b7d0cec602436af3389d0b6f9de3
Sergio Peralta <seperalta88@gmail.com>
2023-06-03 20:02:31 -0300

    create database

---
+++ config/database.py
@@ -0,0 +1,17 @@
+import os
+from unittest.mock import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker  # ??
+from sqlalchemy.ext.declarative import declarative_base
+
+
+sqlite_db_name = "db.sqlite3"
+base_dir = os.path.dirname(os.path.realpath(__file__))
+
+db_url = f"sqlite:///{base_dir}/{sqlite_db_name}"
+
+engine = create_engine(db_url, echo=True)
+
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
